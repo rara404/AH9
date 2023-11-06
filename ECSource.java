3632474b925986de29de9fc823b9629603b1a348
Raj Rana <raj.rana@fiserv.com>
2023-11-06 14:19:46 -0500

    finished!!!

--- ECSource.java
+++ ECSource.java
@@ -1,20 +1,34 @@
-class Node<T> {
-  T val;
-  Node<T> next;
+import java.util.HashMap;
+import java.util.Map;
 
-  public Node(T val) {
-    this.val = val;
-    this.next = null;
-  }
+class Node<T> {
+    T val;
+    Node<T> next;
+
+    public Node(T val) {
+        this.val = val;
+        this.next = null;
+    }
 }
 
 class ECSource {
-  public static <T> boolean isUnivalueList(Node<T> head) {
-    // todo
-    return true;
-  }
 
-  public static void main(String[] args) {
+    static Map<String, Integer> map = new HashMap<>();
+
+    public static <T> boolean isUnivalueList(Node<T> head) {
+
+        while (head != null) {
+            if (map.containsKey(head.val))
+                return false;
+            else
+                map.put((String) head.val, map.getOrDefault(head.val, 0) + 1);
+
+            head = head.next;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
         Node<String> z = new Node<>("z");
         // z
 
--- Source.java
+++ Source.java
@@ -1,33 +1,48 @@
-class Node<T> {
-    T val;
-    Node<T> next;
-    
-    public Node(T val) {
-      this.val = val;
-      this.next = null;
-    }
-  }
-  
-  class Source {
-    public static <T> Node<T> reverseList(Node<T> head) {
-      // todo
-      return head;
-    }
-    
-    public static void main(String[] args) {
-        Node<String> x = new Node<>("x");
-        Node<String> y = new Node<>("y");
-    
-        x.next = y; // x -> y
-    
-        reverseList(x); // y -> x
-
-        // Printing solution
-        Node<String> head = Source.reverseList(x);
-
-        while (head != null) {
-            System.out.println(head.val);
-            head = head.next;            
-        }
-    }
-  }
+//class Node<T> {
+//    T val;
+//    Node<T> next;
+//
+//    public Node(T val) {
+//      this.val = val;
+//      this.next = null;
+//    }
+//  }
+//
+//  class Source {
+//    public static <T>  Node<T> reverseList(Node<T> head)
+//    {
+//            Node prev = null;
+//            Node current = head;
+//            Node next = null;
+//            while (current != null) {
+//                next = current.next;
+//                current.next = prev;
+//                prev = current;
+//                current = next;
+//            }
+//            head = prev;
+//
+//
+//      // todo
+//      return head;
+//    }
+//
+//    public static void main(String[] args) {
+//        Node<String> x = new Node<>("x");
+//        Node<String> y = new Node<>("y");
+//        Node<String> z = new Node<>("z");
+//
+//        x.next = y; // x -> y
+//        y.next = z;
+//
+//        // y -> x
+//
+//        // Printing solution
+//        Node<String> head = Source.reverseList(x);
+//
+//        while (head != null) {
+//            System.out.println(head.val);
+//            head = head.next;
+//        }
+//    }
+//  }
